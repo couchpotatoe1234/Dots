9ab2c15af1b14ee8c3fed05187a00072e9802806
couchpotatoe1234 <leglerlucas@gmail.com>
2026-09-15 20:06:28 -0700

    new animation scripts

--- dots/scripts/player.gd
+++ dots/scripts/player.gd
@@ -4,43 +4,61 @@ extends CharacterBody2D
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 var can_attack = true
-var turning = false
-var trans_run = false
-var running = false
+var current_state = ""
 @onready var sprite = $Sprite2D
 @onready var animation = $AnimationPlayer
 
 func _ready() -> void:
-	animation.play("idle")
+	change_state("idle")
 	
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	var input_dir := 0.0
 	if GameManager.controls_allowed:
 		if Input.is_action_just_pressed("Jump") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
-			animation.play("Jump")
-			running = false
 		if Input.is_action_just_released("Jump") and not is_on_floor():
 			if velocity.y > 0:
 				velocity.y = 0
-		var direction := Input.get_axis("left", "right")
-		if direction:
-			velocity.x = direction * SPEED
-			sprite.flip_h = direction < 0
-			if is_on_floor():
-				if not running:
-					animation.play("run-transition")
-					animation.queue("run")
-					running = true
-				elif animation.current_animation != "run-transition" and animation.current_animation != "run":
-					animation.play("run")
+		
+		input_dir = Input.get_axis("left", "right")
+		if input_dir:
+			velocity.x = input_dir * SPEED
+			sprite.flip_h = input_dir < 0
+
 		else:
-			running = false
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-		if is_on_floor() && not turning && not running:
-			animation.play("idle")
-		#if not is_on_floor() && velocity.y > 0:
-			#animation.play("fall")
+	
 	move_and_slide()
 	
+	update_state(input_dir)
+	
+func update_state(input_dir: float) -> void:
+	if current_state == "run-transition":
+		return
+		
+	if not is_on_floor():
+		if velocity.y < 0:
+			change_state("Jump")
+		else:
+			change_state("fall")
+		return
+		
+	if input_dir != 0:
+		if current_state != "run":
+			change_state("run-transition")
+	else:
+		change_state("idle")
+
+func change_state(new_state: String) -> void:
+	if current_state == new_state:
+		return
+		
+	current_state = new_state
+	animation.play(new_state)
+	
+func _on_animation_finished(anim_name: String) -> void:
+	if anim_name == "run-transition":
+		current_state = "run"
+		animation.play("run")
