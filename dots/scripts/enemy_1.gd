f6d7db9cd0657b6581287741bd7c47adfb7c373a
Oak <aditya.knarr@gmail.com>
2026-09-23 21:36:10 -0700

    enemy finallyyy worksss now I have to make health...

--- dots/scripts/enemy_1.gd
+++ dots/scripts/enemy_1.gd
@@ -12,22 +12,26 @@ var direction: int = 1
 
 func _physics_process(delta: float) -> void:
 	print("tick", velocity)
+	print("wall: ", wall_ray.is_colliding(), " ledge: ", ledge_ray.is_colliding())
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
+	#Turn around code
+	if is_on_wall() or (wall_ray.is_colliding() or not ledge_ray.is_colliding()):
+		flip_direction()
+	
 	velocity.x = direction * speed
 	
-	#Turn around code
-	if is_on_wall() or (wall_ray.is_colliding() and not ledge_ray.is_colliding()):
-		flip_direction()
-		
 	move_and_slide()
 
 func flip_direction() -> void:
 	direction *= -1
 	sprite.flip_h = (direction > 0)
 	wall_ray.scale.x = -wall_ray.scale.x
+	wall_ray.position.x = -wall_ray.position.x
 	ledge_ray.scale.x = -ledge_ray.scale.x
+	ledge_ray.position.x = -ledge_ray.position.x
 	
 func _ready():
 	sprite.play("walk")
+	sprite.flip_h = (direction > 0)
